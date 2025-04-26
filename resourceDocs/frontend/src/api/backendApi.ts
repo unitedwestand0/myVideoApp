bf3e49309e32dcbfd78efd66203b3f16ebdf3661
Rachel G Ramsey <46530671+unitedwestand0@users.noreply.github.com>
2025-04-26 18:14:25 -0500

    Delete resourceDocs directory

--- resourceDocs/frontend/src/api/backendApi.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const backendApi = axios.create({
-  // baseURL: "http://localhost:8000",
-  baseURL: "https://my-video-hub-backend-api.vercel.app",
-});
-export default backendApi;
--- resourceDocs/frontend/src/customHooks/useConfigHook.ts
+++
@@ -1,9 +0,0 @@
-export const useConfig = () => {
-  const token = localStorage.getItem("token");
-  const configWithJWT = {
-    headers: {
-      Authorization: `Bearer ${token}`, // Pass the token in the header
-    },
-  };
-  return { configWithJWT };
-};
